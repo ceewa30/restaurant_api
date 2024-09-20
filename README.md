4fd214ebb6789d93aa3601b1711eedcd4ee6bfa2
ceewa30 <ceewa30@gmail.com>
2024-09-20 10:58:43 -0400

    Test email is normalized for new users.

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ docker-compose up
 docker-compose run --rm app sh -c "python manage.py test"
 
 docker-compose run --rm app sh -c "python manage.py startapp core"
+
+docker-compose run --rm app sh -c "python manage.py makemigrations"
+
+docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"
+
+docker volume ls
+
+docker volume rm restaurant_api_dev-db-data
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
-# from django.db import models
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for User model."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new User."""
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
